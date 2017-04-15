420e716f49e95d2e5457fcfb8c178e40476dc2f6
Lucas <pachobo16@hotmail.fr>
2017-04-15 16:47:13 +0200

    Nouvelle version du logGenerator

--- src/projet/coordinateur/LogGeneratorTest.java
+++ src/projet/coordinateur/LogGeneratorTest.java
@@ -2,7 +2,6 @@ package projet.coordinateur;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.RandomAccessFile;
 import java.util.Random;
 
 /**
@@ -13,127 +12,71 @@ public class LogGeneratorTest {
     public static void main(String[] args){
 
         Random r = new Random();
-        int total1 = 0;
-        int total2 = 0;
-        int total3 = 0;
-        int total4 = 0;
-        int total5 = 0;
-        int target1 = 300;
-        int target2 = 225;
-        int target3 = 200;
-        int target4 = 350;
-        int target5 = 275;
+
+        int nbJoueurs = 5;
+        int nbResources = 5;
+
+        int[] total = new int[nbResources];
+        int[] target = new int[nbResources];
+
+        for (int i = 0; i < target.length; i++){
+            target[i] = r.nextInt(150)+200;
+        }
 
         int cpt;
 
-        for (int i = 0; i<5; i++){
+        for (int i = 0; i < nbJoueurs; i++){
+
             cpt = 0;
-            total1 = 0;
-            total2 = 0;
-            total3 = 0;
-            total4 = 0;
-            total5 = 0;
+
+            for (int j = 0; j < total.length; j++){
+                total[j] = 0;
+            }
+
             try{
                 PrintWriter w = new PrintWriter("log_"+i+".txt", "UTF-8");
-                w.println("JOUEUR");
                 w.println("Joueur " + i);
-                w.println("ACTIONS");
 
-                while (total1 < target1 || total2 < target2 || total3 < target3 || total4 < target4 || total5 < target5) {
+                boolean resMissing = true;
+                boolean[] b = new boolean[nbResources];
+                for (int j = 0; j < b.length; j++){
+                    b[j] = true;
+                }
+
+                while(resMissing) {
                     cpt++;
                     int k = r.nextInt(5);
                     int demande, recu;
-                    switch (k){
-                        case 0:
-                            if (total1 < target1){
-                                if (target1-total1 < 50){
-                                    demande = target1-total1;
-                                    recu = demande;
-                                }
-                                else {
-                                    demande = 50;
-                                    recu = r.nextInt(demande);
-                                }
-                                total1 += recu;
-                                w.println(cpt + " get " + (r.nextInt(3)+1) + " " +(k+1) + " " + demande + " " + recu +
-                                        " " + total1 + " " + total2 + " " + total3 + " " + total4 + " " + total5);
+                    if (total[k] < target[k]){
+                        if (target[k] - total[k] < 50){
+                            demande = target[k] - total[k];
+                            recu = demande;
+                        }
+                        else {
+                            demande = 50;
+                            recu = r.nextInt(demande);
+                        }
+                        total[k] += recu;
+
+                        w.print(cpt + " get " + (r.nextInt(3)+1) + " " +(k+1) + " " + demande + " " + recu);
+                        for (int j = 0; j < nbResources; j++){
+                            w.print(" " + total[j]);
+                        }
+                        w.println();
+
+                        if (total[k] >= target[k])
+                            b[k] = false;
+
+                        resMissing = false;
+                        for (int j = 0; j < b.length; j++){
+                            if (b[j]){
+                                resMissing = true;
                             }
-                            else{
-                                cpt--;
-                            }
-                            break;
-                        case 1:
-                            if (total2 < target2){
-                                if (target2-total2 < 50){
-                                    demande = target2-total2;
-                                    recu = demande;
-                                }
-                                else {
-                                    demande = 50;
-                                    recu = r.nextInt(demande)+1;
-                                }
-                                total2 += recu;
-                                w.println(cpt + " get " + (r.nextInt(3)+1) + " " +(k+1) + " " + demande + " " + recu +
-                                        " " + total1 + " " + total2 + " " + total3 + " " + total4 + " " + total5);
-                            }
-                            else{
-                                cpt--;
-                            }
-                            break;
-                        case 2:
-                            if (total3 < target3){
-                                if (target3-total3 < 50){
-                                    demande = target3-total3;
-                                    recu = demande;
-                                }
-                                else {
-                                    demande = 50;
-                                    recu = r.nextInt(demande)+1;
-                                }
-                                total3 += recu;
-                                w.println(cpt + " get " + (r.nextInt(3)+1) + " " +(k+1) + " " + demande + " " + recu +
-                                        " " + total1 + " " + total2 + " " + total3 + " " + total4 + " " + total5);
-                            }
-                            else{
-                                cpt--;
-                            }
-                            break;
-                        case 3:
-                            if (total4 < target4){
-                                if (target4-total4 < 50){
-                                    demande = target4-total4;
-                                    recu = demande;
-                                }
-                                else {
-                                    demande = 50;
-                                    recu = r.nextInt(demande)+1;
-                                }
-                                total4 += recu;
-                                w.println(cpt + " get " + (r.nextInt(3)+1) + " " +(k+1) + " " + demande + " " + recu +
-                                        " " + total1 + " " + total2 + " " + total3 + " " + total4 + " " + total5);
-                            }
-                            else{
-                                cpt--;
-                            }
-                            break;
-                        case 4:
-                            if (total5 < target5){
-                                if (target5-total5 < 50){
-                                    demande = target5-total5;
-                                    recu = demande;
-                                }
-                                else {
-                                    demande = 50;
-                                    recu = r.nextInt(50)+1;
-                                }
-                                total5 += recu;
-                                w.println(cpt + " get " + (r.nextInt(3)+1) + " " +(k+1) + " " + demande + " " + recu +
-                                        " " + total1 + " " + total2 + " " + total3 + " " + total4 + " " + total5);
-                            }
-                            else{
-                                cpt--;
-                            }
-                            break;
+                        }
+
+                    }
+                    else{
+                        cpt--;
                     }
                 }
 
